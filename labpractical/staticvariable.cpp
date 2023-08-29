74c609fbf278ef52ebf61ff63ee5508cc6da0136
Priyankashrestha16 <priyankashrestha690@gmail.com>
2023-08-29 13:41:35 +0545

    firstcommit

--- labpractical/staticvariable.cpp
+++ labpractical/staticvariable.cpp
@@ -1,18 +1,33 @@
 #include<iostream>
 using namespace std;
-class statics{
+class part{
     private:
-    static int num;
+    static int count;
+    int pid;
     public:
-   
-  void func(){
-    cout<<"value of num is:"<<num<<endl;
-}
+    part(){
+        count ++;
+        pid=count;
+    }
+    int getcount(){
+        return count;
+    }
+    void show(){
+        cout<<"parts id:"<<pid<<endl;
+        cout<<"total number of parts are:"<<count<<endl;
+    }
 };
+int part :: count=0;
 int main(){
-    statics s1,s2;
-    s1.num=1;
-
-    s1.func();
-
+    part p1,p2;
+    cout<<"count is:"<<p1.getcount()<<endl;
+    p1.show();
+    cout<<"count is:"<<p2.getcount()<<endl;
+    p2.show();
+    part p3;
+    cout<<"count is:"<<p3.getcount()<<endl;
+    p3.show();
+    cout<<"count is:"<<p1.getcount()<<endl;
+    p1.show();
+    return 0;
 }
